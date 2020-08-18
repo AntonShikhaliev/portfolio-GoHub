a9fd0052cb104d4f41745f6dd582e661b741470a
AntonShikhaliev <50765540+AntonShikhaliev@users.noreply.github.com>
2020-08-19 00:41:41 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.querySelector(".menu__burger").addEventListener("click", function () {
+ document.querySelector(".menu__burger").classList.toggle("menu__burger-activ");
+ document.querySelector(".menu").classList.toggle("menu-mobi");
+});
